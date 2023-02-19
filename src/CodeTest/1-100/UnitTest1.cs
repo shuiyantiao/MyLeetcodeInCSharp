a432a0d31cd72fe6b6541f19d7ab7086c7d4c3d2
shuiyantiao <shuiyantiao@foxmail.com>
2023-02-20 00:34:42 +0800

    Commit project and test

---
+++ src/CodeTest/1-100/UnitTest1.cs
@@ -0,0 +1,30 @@
+﻿namespace MyLeetcodeInCSharp._1_100.Tests
+{
+    [TestClass()]
+    public class UnitTest1
+    {
+        [TestMethod()]
+        public void TwoSumTest1()
+        {
+            var t = new _1();
+            var result = t.TwoSum(new int[] { 2, 7, 11, 15 }, 9);
+            CollectionAssert.AreEqual(new int[] { 0, 1 }, result);
+        }
+
+        [TestMethod()]
+        public void TwoSumTest2()
+        {
+            var t = new _1();
+            var result = t.TwoSum(new int[] { 3, 2, 4 }, 6);
+            CollectionAssert.AreEqual(new int[] { 1, 2 }, result);
+        }
+
+        [TestMethod()]
+        public void TwoSumTest3()
+        {
+            var t = new _1();
+            var result = t.TwoSum(new int[] { 3, 3 }, 6);
+            CollectionAssert.AreEqual(new int[] { 0, 1 }, result);
+        }
+    }
+}
---
+++ src/MyLeetcodeInCSharp/1-100/1.cs
@@ -0,0 +1,19 @@
+﻿namespace MyLeetcodeInCSharp._1_100
+{
+    public class _1
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            Dictionary<int, int> targetNums = new();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (targetNums.TryGetValue(nums[i], out int pos))
+                {
+                    return new int[] { pos, i };
+                }
+                targetNums.TryAdd(target - nums[i], i);
+            }
+            return Array.Empty<int>();
+        }
+    }
+}
